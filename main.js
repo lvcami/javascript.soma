00b28b282da3ad94a4ce221e7d588337079d58d2
lvcami <163419225+lvcami@users.noreply.github.com>
2024-11-07 09:55:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let numero1= parseInt(prompt("informe o primeiro numero: "));
+let numero2= parseInt(prompt("informe o segundo numero: "));
+let soma = numero1 + numero2;
+
+document.write("a soma é igual a: ", soma);
+
+console.log("o valor da soma no console é igual a: ", soma);
